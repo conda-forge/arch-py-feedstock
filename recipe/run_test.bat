6345d621d92f38c8384d486c0226a7ba935779da
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2023-05-26 10:29:54 +0100

    Change tests

--- recipe/run_test.bat
+++
@@ -1,5 +0,0 @@
-set OPENBLAS_NUM_THREADS=1
-set MKL_NUM_THREADS=1
-set OMP_NUM_THREADS=1
-
-python -c "import arch; arch.test(['-n','auto','--durations=25'])"
--- recipe/run_test.sh
+++
@@ -1,5 +0,0 @@
-export OPENBLAS_NUM_THREADS=1
-export MKL_NUM_THREADS=1
-export OMP_NUM_THREADS=1
-
-python -c "import arch; arch.test(['-n','auto','--durations=25'])"
